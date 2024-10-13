55a71e18ecc4e71c615e9ea8dd068e1657d9a4e7
Jake Lazaroff <yo@jake.nyc>
2024-10-13 18:00:18 -0400

    Set up planes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,56 @@
 "use client";
 
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import type * as THREE from "three";
+import React, { useRef } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stats, OrbitControls, TransformControls } from "@react-three/drei";
 
-function Box(props: { position: [x: number, y: number, z: number] }) {
-  // This reference will give us direct access to the mesh
-  const meshRef = useRef<THREE.Mesh>(null!!!);
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
-  // Return view, these are regular three.js elements expressed in JSX
+import * as THREE from "three";
+
+const floor: THREE.Vector3[] = [
+  new THREE.Vector3(-2, -4, -2),
+  new THREE.Vector3(2, -4, -2),
+  new THREE.Vector3(2, -4, 2),
+  new THREE.Vector3(-2, -4, 2)
+];
+
+const ceiling: THREE.Vector3[] = [
+  new THREE.Vector3(-3, 4, 0),
+  new THREE.Vector3(0, 4, 3),
+  new THREE.Vector3(3, 4, 0),
+  new THREE.Vector3(0, 4, -3)
+];
+
+function Box(props: { position: THREE.Vector3 }) {
+  const ref = useRef<THREE.Mesh>(null!!!);
+
   return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={() => setActive(!active)}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    <mesh {...props} ref={ref}>
+      <sphereGeometry args={[0.25, 32, 16]} />
+      <meshStandardMaterial color="blue" />
     </mesh>
   );
 }
 
 export default function Home() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 20, 20] }}>
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+
+      <group>
+        {floor.map((point, i) => (
+          <Box key={i} position={point} />
+        ))}
+      </group>
+      <group>
+        {ceiling.map((point, i) => (
+          <Box key={i} position={point} />
+        ))}
+      </group>
+
+      <OrbitControls />
+      <Stats />
     </Canvas>
   );
 }
